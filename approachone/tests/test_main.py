2d245c8e22b07e4db9f1d3a9fe62abeb317b60b7
Douglas Kaiser <51721139+douglastkaiser@users.noreply.github.com>
2026-07-04 23:12:01 +0000

    Moer cleanup

--- approachone/tests/test_main.py
+++ approachone/tests/test_main.py
@@ -4,3 +4,9 @@ from approachone.main import allocate
 def test_allocate():
     w0, w1, w2, w3, w4, w5, w6, w7 = allocate(0, 0, 0)
     assert w0 == w1 == w2 == w3 == w4 == w5 == w6 == w7 == 0
+
+    w0, w1, w2, w3, w4, w5, w6, w7 = allocate(0, 0, -100)
+    assert w0 == w1 == w2 == w3 == w4 == w5 == w6 == w7 == 0
+
+    w0, w1, w2, w3, w4, w5, w6, w7 = allocate(0, 0, 100)
+    assert w0 == w1 == w2 == w3 == w4 == w5 == w6 == w7
