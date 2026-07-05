6d1728fe782f832d32414c21bb245907e1819b23
Douglas Kaiser <51721139+douglastkaiser@users.noreply.github.com>
2026-07-05 05:54:32 +0000

    sim and contrllers

--- approachone/main.py
+++ approachone/main.py
@@ -95,3 +95,68 @@ def allocate(
     w7 = max(w7, 0)
 
     return w0, w1, w2, w3, w4, w5, w6, w7
+
+
+def sim(w0, w1, w2, w3, w4, w5, w6, w7, pitch_rate, yaw_rate):
+    dt = 0.01
+
+    C = 1
+    F0 = C * w0**2
+    F1 = C * w1**2
+    F2 = C * w2**2
+    F3 = C * w3**2
+    F4 = C * w4**2
+    F5 = C * w5**2
+    F6 = C * w6**2
+    F7 = C * w7**2
+
+    T = F0 + F1 + F2 + F3 + F4 + F5 + F6 + F7
+    # \(T = D = \frac{1}{2} \rho V^2 S C_D\)
+    C = 1
+    airspeed = math.sqrt(T / C)
+
+    r0_z = r1_z = r2_z = r3_z = -1
+    r4_z = r5_z = r6_z = r7_z = 1
+    torque_y = (
+        F0 * r0_z
+        + F1 * r1_z
+        + F2 * r2_z
+        + F3 * r3_z
+        + F4 * r4_z
+        + F5 * r5_z
+        + F6 * r6_z
+        + F7 * r7_z
+    )
+    I_y = 1
+    pitch_accel = torque_y / I_y
+    pitch_rate = pitch_rate + pitch_accel * dt
+
+    r0_y = r4_y = 1.5
+    r1_y = r5_y = 0.5
+    r2_y = r6_y = -r1_y
+    r3_y = r7_y = -r2_y
+    torque_z = (
+        F0 * r0_y
+        + F1 * r1_y
+        + F2 * r2_y
+        + F3 * r3_y
+        + F4 * r4_y
+        + F5 * r5_y
+        + F6 * r6_y
+        + F7 * r7_y
+    )
+    I_z = 1
+    yaw_accel = torque_z / I_z
+    yaw_rate = yaw_rate + yaw_accel * dt
+
+    return pitch_rate, yaw_rate, airspeed
+
+
+def controllers(
+    pitch_rate, pitch_rate_cmd, yaw_rate, yaw_rate_cmd, airspeed, airspeed_cmd
+):
+    kp = 1
+    torque_y_cmd = kp * (pitch_rate_cmd - pitch_rate)
+    torque_z_cmd = kp * (yaw_rate_cmd - yaw_rate)
+    thrust_cmd = kp * (airspeed_cmd - airspeed)
+    return torque_y_cmd, torque_z_cmd, thrust_cmd
--- approachone/tests/test_main.py
+++ approachone/tests/test_main.py
@@ -1,5 +1,5 @@
 from approachone.main import allocate
-# from common.utils import controllers
+
 
 def test_allocate():
     w0, w1, w2, w3, w4, w5, w6, w7 = allocate(0, 0, 0)
