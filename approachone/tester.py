a25463e1d5a8e24dc52e14e7af4a9e5d17e8f6a9
Douglas Kaiser <51721139+douglastkaiser@users.noreply.github.com>
2026-07-09 01:24:27 +0000

    Tester commit

--- approachone/tester.py
+++ approachone/tester.py
@@ -17,16 +17,21 @@ r_quadrant_1_z = -1
 r_quadrant_2_z = 1
 r_quadrant_3_z = 1
 
-# t = 0
-x = [1, 0, 0]
-x_cmd = [0, 0, 0]
+x_cmd = [0, 0, 10]
 integrals = [0, 0, 0]
 prev_deltas = [0, 0, 0]
-gains = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+gains = [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+# t = 0
+print("t = 0")
+pitch_rate, yaw_rate, u_air = [1, 0, 0]
+print(f" {pitch_rate=}, {yaw_rate=}, {u_air=}")
+x = [pitch_rate, yaw_rate, u_air]
 u, integrals, prev_deltas = utils.controllers(
     x, x_cmd, integrals, prev_deltas, gains, dt
 )
 tau_y, tau_z, thrust = u
+print(f" {tau_y=}, {tau_z=}, {thrust=}")
 w0, w1, w2, w3, w4, w5, w6, w7 = allocate(
     tau_y,
     tau_z,
@@ -41,4 +46,7 @@ w0, w1, w2, w3, w4, w5, w6, w7 = allocate(
     r_quadrant_2_z,
     r_quadrant_3_z,
 )
+print(f" {w0=}, {w1=}, {w2=}, {w3=}, {w4=}, {w5=}, {w6=}, {w7=}")
 pitch_rate, yaw_rate, u_air = sim.sim(w0, w1, w2, w3, w4, w5, w6, w7, 0, 0, 0, C, dt)
+print("t = 1")
+print(f" {pitch_rate=}, {yaw_rate=}, {u_air=}")
